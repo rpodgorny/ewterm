2dd06484ac01645760b6cdeacbf97b65a9a4da28
Radek Podgorny <radek@podgorny.cz>
2006-11-14 12:55:53 +0100

    More refactoring

--- x25_packet.c
+++ x25_packet.c
@@ -7,54 +7,27 @@
 
 
 int packet_serialize(struct packet *p, unsigned char *buf) {
-	int pre_size = preamble_size(p->pre);
-	int b_size = block_size(p->data);
+	int pre_size = 11;
+	int b_size = 0;
+	if (p->data) block_size(p->data);
 
-	preamble_serialize(p->pre, buf);
-	block_serialize(p->data, buf+pre_size);
+	*buf = p->family;
+	*(buf+1) = p->unk1;
+	*(buf+2) = p->dir;
+	*(buf+3) = p->pltype;
+	*(unsigned short *)(buf+4) = htons(p->connid);
+	*(buf+6) = p->subseq;
+	*(buf+7) = p->unk2;
+	*(unsigned short *)(buf+8) = htons(p->unk3);
+	*(buf+10) = p->tail;
+
+	if (p->data) block_serialize(p->data, buf+pre_size);
 
 	return pre_size+b_size;
 }
 
-int preamble_size(struct preamble *p) {
-	return 11;
-}
-
-struct packet *packet_deserialize(char *buf) {
+struct packet *packet_deserialize(unsigned char *buf) {
 	struct packet *ret = malloc(sizeof(struct packet));
-
-	ret->pre = preamble_deserialize(buf);
-	ret->data = block_deserialize(buf+11, NULL);
-
-	return ret;
-}
-
-void packet_print(struct packet *p) {
-	preamble_print(p->pre);
-	block_print(p->data);
-}
-
-void preamble_delete(struct preamble *p) {
-	if (!p) return;
-
-	free(p);
-}
-
-void packet_delete(struct packet *p) {
-	if (!p) return;
-
-	preamble_delete(p->pre);
-	block_delete(p->data);
-	free(p);
-}
-
-void preamble_print(struct preamble *p) {
-	printf("fam: %x, unk1: %x, dir: %x, pltype: %x, connid: %x, subseq: %x, unk2: %x, unk3: %x, tail: %x\n", p->family, p->unk1, p->dir, p->pltype, p->connid, p->subseq, p->unk2, p->unk3, p->tail);
-}
-
-
-struct preamble *preamble_deserialize(char *buf) {
-	struct preamble *ret = malloc(sizeof(struct preamble));
 
 	ret->family = *buf;
 	ret->unk1 = *(buf+1);
@@ -66,17 +39,18 @@ struct preamble *preamble_deserialize(char *buf) {
 	ret->unk3 = ntohs(*(unsigned short *)(buf+8));
 	ret->tail = *(buf+10);
 
+	ret->data = block_deserialize(buf+11, NULL);
+
 	return ret;
 }
 
-void preamble_serialize(struct preamble *p, char *buf) {
-	*buf = p->family;
-	*(buf+1) = p->unk1;
-	*(buf+2) = p->dir;
-	*(buf+3) = p->pltype;
-	*(unsigned short *)(buf+4) = htons(p->connid);
-	*(buf+6) = p->subseq;
-	*(buf+7) = p->unk2;
-	*(unsigned short *)(buf+8) = htons(p->unk3);
-	*(buf+10) = p->tail;
+void packet_print(struct packet *p) {
+	printf("fam: %x, unk1: %x, dir: %x, pltype: %x, connid: %x, subseq: %x, unk2: %x, unk3: %x, tail: %x\n", p->family, p->unk1, p->dir, p->pltype, p->connid, p->subseq, p->unk2, p->unk3, p->tail);
+
+	if (p->data) block_print(p->data);
+}
+
+void packet_delete(struct packet *p) {
+	if (p->data) block_delete(p->data);
+	free(p);
 }
