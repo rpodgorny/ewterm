19f8b866f6425e66eadb31c6877c19633921e17b
Radek Podgorny <radek@podgorny.cz>
2006-12-19 15:13:18 +0100

    Added to_bcd() declaration to header

--- x25_utils.h
+++ x25_utils.h
@@ -3,6 +3,7 @@
 
 #include "x25_packet.h"
 
+int to_bcd(unsigned char *, char *);
 struct packet *login_packet(char *, char *);
 struct packet *command_packet(char *, int);
 struct packet *command_confirmation_packet(unsigned short, unsigned short, unsigned char, char *, int);
